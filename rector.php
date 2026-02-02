ff26efb80ae0bbd376e21a49804c8cac43f02c8c
Adil Azhari <adelazhry1997@gmail.com>
2026-02-03 01:38:36 +0800

    chore: update Rector configuration

--- rector.php
+++ rector.php
@@ -3,24 +3,44 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\TypeDeclaration\Rector\ArrowFunction\AddArrowFunctionReturnTypeRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedCallRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnUnionTypeRector;
+use Rector\TypeDeclaration\Rector\Closure\AddClosureVoidReturnTypeWhereNoReturnRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/bootstrap/app.php',
-        __DIR__.'/database',
+        __DIR__.'/bootstrap',
+        __DIR__.'/config',
         __DIR__.'/public',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
     ])
     ->withSkip([
-        AddOverrideAttributeToOverriddenMethodsRector::class,
+        __DIR__.'/bootstrap/cache',
+        __DIR__.'/storage',
+        __DIR__.'/vendor',
+        AddClosureVoidReturnTypeWhereNoReturnRector::class,
+        ReturnTypeFromStrictTypedCallRector::class,
+        ReturnUnionTypeRector::class,
+        DeclareStrictTypesRector::class => [
+            __DIR__.'/resources/views',
+        ],
+        AddArrowFunctionReturnTypeRector::class,
     ])
+    ->withPhpSets()
+    ->withImportNames()
+    ->withComposerBased(laravel: true)
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true,
         typeDeclarations: true,
         privatization: true,
         earlyReturn: true,
-        strictBooleans: true,
     )
-    ->withPhpSets();
+    ->withRules([
+        DeclareStrictTypesRector::class,
+    ]);
